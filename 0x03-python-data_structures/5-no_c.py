9cc032c16cf3ab24ef49cb1b2524373a60265f4e
Andrea29lwehumbiza <andrealwehumbiza@gmail.com>
2023-12-04 12:58:58 -0800

    5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
-i!/usr/bin/env python3
-no_c = __import__('5-no_c').no_c
-
-print(no_c("Best School"))
-print(no_c("Chicago"))
-print(no_c("C is fun!"))
-
+#!/usr/bin/python3
+def no_c(my_string):
+    new_string = ""
+    for elements in my_string:
+        if elements != "c" and elements != "C":
+            new_string += elements
+    return new_string
